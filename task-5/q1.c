d4fcbc011a3ed3758fa963b0af688ad983d10151
lokhacodes <sumaiyasonali421@gmail.com>
2023-02-07 12:50:21 +0600

    task-5

---
+++ task-5/q1.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include <stdlib.h>
+int top = -1, stack [100];
+int main ( ){
+   char a[50], ch;
+   int i,op1,op2,res,x;
+   void push (int);
+   int pop( );
+   int eval (char, int, int);
+   printf("enter a postfix expression:");
+   gets (a);
+   for(i=0; a[i]!='\0'; i++){
+      ch = a[i];
+      if (ch>='0' && ch<='9'){
+        int num = ch-'0';
+  //  printf("%d",num);
+         push(num);}
+      else{
+         op2 = pop ( );
+         op1 = pop ( );
+         res = eval (ch, op1, op2);
+         push (res);
+      }
+   }
+   x = pop ( );
+   printf("evaluated value = %d", x);
+  //getch ( );
+}
+void push (int n){
+   top++;
+   stack [top] = n;
+}
+int pop ( ){
+   int res ;
+   res = stack [top];
+   top--;
+   return res;
+}
+int eval (char ch, int op1, int op2){
+   switch (ch){
+      case '+' : return (op1+op2);
+      case '-' : return (op1-op2);
+      case '*' : return (op1*op2);
+      case '/' : return (op1/op2);
+      case')' : exit(1);
+    //  case '^' : return (op2^op2);
+   }
+return 0;
+}
---
+++ task-5/readme.md
@@ -0,0 +1,3 @@
+Evaluate any postfix notation where you can use only a digit for a number.
+Sample input: 5,6,2,+,*,8,4,/,-,)
+Output: 38
